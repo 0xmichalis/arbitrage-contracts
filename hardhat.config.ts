7344e0b0faff163eeb064aa73447d7396de66141
Michalis Kargakis <kargakis@protonmail.com>
2022-01-07 22:46:53 +0100

    Ropsten config and yarn clean

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -35,6 +35,11 @@ const config: HardhatUserConfig = {
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
+    ropsten: {
+      url: process.env.NODE_API_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
